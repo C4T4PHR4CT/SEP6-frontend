833ccb6976464dc4f377acee92a7b606c926a6dc
Audrius <azatas25@gmail.com>
2022-05-11 16:14:46 +0200

    Changed from sass to scss, setup modules
    Changed to scss, due to errors with sass complier and setup a very basic module structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatSliderModule } from '@angular/material/slider';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule ],
+    SharedModule],
   providers: [],
   bootstrap: [AppComponent]
 })
