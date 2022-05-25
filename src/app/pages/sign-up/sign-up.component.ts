d4871312fb6c5727f3bd67cd8e7b10a71f17f5b0
Audrius <azatas25@gmail.com>
2022-05-25 17:37:58 +0200

    setting up login

---
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/auth/auth.service';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss']
+})
+export class SignUpComponent implements OnInit {
+  signupForm: FormGroup;
+  constructor(
+    public fb: FormBuilder,
+    public authService: AuthService,
+    public router: Router
+  ) {
+    this.signupForm = this.fb.group({
+      username: [''],
+      email: [''],
+      password: [''],
+    });
+  }
+  ngOnInit() {}
+  registerUser() {
+    this.authService.signUp(this.signupForm.value).subscribe((res) => {
+      if (res.result) {
+        this.signupForm.reset();
+        this.router.navigate(['log-in']);
+      }
+    });
+  }
+}
