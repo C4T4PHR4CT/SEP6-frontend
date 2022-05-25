c2b108ec77035a6d10e65a6bb45230566f3dcfda
Audrius <azatas25@gmail.com>
2022-05-25 14:54:27 +0200

    sussy
    Improving module structure

--- src/app/pages/movies/movies.module.ts
+++ src/app/pages/movies/movies.module.ts
@@ -1,3 +1,4 @@
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { NgModule } from '@angular/core';
 import { MoviesService } from 'src/app/shared/services/movies.service';
 import { SharedModule } from 'src/app/shared/shared.module';
@@ -6,9 +7,10 @@ import { MoviesRoutingModule } from './movies-routing.module';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { SearchComponent } from './search/search.component';
 import { DiscoverComponent } from './discover/discover.component';
+import { MoviesComponent } from './movies.component';
 
 @NgModule({
-  declarations: [DiscoverComponent, MovieComponent, SearchComponent],
+  declarations: [DiscoverComponent, MovieComponent, SearchComponent, MoviesComponent],
   imports: [SharedModule, MoviesRoutingModule, NgxPaginationModule],
   providers: [MoviesService],
 })
